56440d0ac5ac609b35a8426445e635b1ab1d3b87
Paulo H. Leocadio <22998694+paulohl@users.noreply.github.com>
2023-05-20 20:31:59 -0400

    Add files via upload

---
+++ subsidiaries.php
@@ -0,0 +1,116 @@
+<?php
+    include("xyz_index_header.php");
+    require_once 'includes/config.php'; 
+?>
+
+<div style="margin-right:auto;
+            margin-left:auto;
+            width:980px;
+            clear:both;
+            padding-top:50px;
+            maring-bottom:50px;">
+    <p style="font-family:giorgio;
+              font-size:31px;
+              color:#555f67;
+              line-height:41px;
+              margin-bottom:50px" >
+        sculpIT.xyz divisions:  
+        <br/>
+        fully owned subsidiaries and equity investments  
+    </p>
+    
+    <p id="sculpIT.vc">
+        <a href="http://www.sculpit.vc/">
+            <img src="/images/sculpIT_LOGO_D_banner.png" width="200px"/>
+        </a>
+    </p>   
+    <div style="margin-right:auto;
+            margin-left:auto;
+            width:980px;
+            clear:both;
+            padding-top:5px;
+            maring-bottom:50px;">
+    <p style="font-family:giorgio;
+              font-size:20px;
+              color:#555f67;
+              line-height:30px;
+              margin-bottom:20px" >
+        sculpIT Venture Capital our private equity arm
+        <br/>
+        focused on new technologies and solutions for health and fitness
+    </p>
+    
+    
+    <p id="sculpIT.shop">
+        <a href="http://www.sculpit.auction/">
+            <img src="/images/sculpIT_LOGO_A_banner.png" width="200px"/>
+        </a>
+    </p>   
+    <div style="margin-right:auto;
+            margin-left:auto;
+            width:980px;
+            clear:both;
+            padding-top:5px;
+            maring-bottom:50px;">
+    <p style="font-family:giorgio;
+              font-size:20px;
+              color:#555f67;
+              line-height:30px;
+              margin-bottom:20px" >
+        sculpIT eCommerce Edition  
+        <br/>
+        auctions, haggle, bargains, offers, gold & silver bullions, oddities
+    </p>
+    
+    
+     <p id="POWERHANDZ">
+        <a href="https://powerhandz.com">
+            <img src="/images/POWERHANDZ-Logo-Dark1.jpg" width="200px"/>
+        </a>
+    </p>   
+    <div style="margin-right:auto;
+            margin-left:auto;
+            width:980px;
+            clear:both;
+            padding-top:5px;
+            maring-bottom:50px;">
+    <p style="font-family:giorgio;
+              font-size:20px;
+              color:#555f67;
+              line-height:30px;
+              margin-bottom:20px" >
+        Empowering athletes and patients in the world to train with purpose  
+        <br/>
+    </p>
+    
+    
+    <p id="sculpit.fitness">
+        <a href="index-fit.php">
+            <img src="./images/sculpit_fitness_logo.png" width="200px"/>
+        </a>
+    </p>
+ <p style="padding: 10px;"></p>
+<div style="width:425px;float:left;">	
+</div>
+<div style="width:425px;float:left;">	
+<iframe width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/uei7yrwQz4Y" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; 
+               autoplay; 
+               clipboard-write; 
+               encrypted-media; 
+               gyroscope; 
+               picture-in-picture; 
+               web-share" 
+        allowfullscreen>
+</iframe>
+
+</div>
+
+<?php
+include("xyz_index_footer.php");
+?>
+
