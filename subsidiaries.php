1afe875bd0262f6f97104189d06c3083e0034a54
Paulo H. Leocadio <22998694+paulohl@users.noreply.github.com>
2023-05-22 14:34:43 -0400

    Update subsidiaries.php

--- subsidiaries.php
+++ subsidiaries.php
@@ -59,19 +59,19 @@
 						sculpIT.xyz divisions: </br>
 						fully owned subsidiaries and equity investments
 					</p>
-					<p id="sculpIT.vc;
-					        font-family:giorgio;
-					       	font-size:31px;
-                                              	color:#555f67;
-                                              	line-height:41px;
-                                              	margin-bottom:50px" >
-						<a href="http://www.sculpit.vc/">
-							<img src="/images/sculpIT_LOGO_D_banner.png" width="200px">
-						</a></br>
-							<p>sculpIT Venture Capital our private equity arm<br/>focused on new technologies and solutions for health and fitness
-	</p></br>
-					</p>
-					<p style="font-family:giorgio;
+			<p id="sculpIT.vc;
+			       font-family:Giorgio;
+			       font-size:31px;
+			       color:#555f67;
+			       line-height:41px;
+			       margin-bottom:50px">
+				<a href="http://www.sculpit.vc/">
+					<img src="/images/sculpIT_LOGO_D_banner.png" width="200px">
+				</a></br>
+			<p>sculpIT Venture Capital our private equity arm<br/>focused on new technologies and solutions for health and fitness
+			</p></br>
+			</p>
+					<p style="font-family:Giorgio;
               					font-size:20px;
               					color:#555f67;
               					line-height:30px;
@@ -80,7 +80,7 @@
 							<img src="/images/sculpIT_LOGO_B_banner.png" width="200px">
 						</a></br>
 							<p>sculpIT eCommerce Edition </br>auctions, haggle, bargains, offers, gold & silver bullions, oddities
-</p></br>
+							</p></br>	
 					</p>
 					<p style="font-family:Giorgio;
               					font-size:20px;
@@ -91,7 +91,7 @@
 							<img src="/images/POWERHANDZ-Logo-Dark1.jpg" width="200px">
 						</a></br>
 							<p>POWERHANDZ©: Empowering athletes and patients in the world to train with purpose
-</p></br>					
+							</p></br>					
 					</p>					
 					<p style="font-family:Giorgio;
               					font-size:20px;
@@ -102,39 +102,43 @@
 							<img src="./images/sculpit_fitness_logo.png" width="200px">
 						</a></br>
 							<p>sculpIT.Fitness: Your Personal Trainer at your fingertips
-</p></br>					
+							</p></br>					
 					</p>
 
 					<p style="padding: 10px;">
 					</p>
-				</div>
-
-				</div><div style="width:425px;
-					inline-end;">
-					<iframe type "align=center" 
-						width="560" 
-						height="315" 
+		</div>
+			<div style="width:425px; 
+	    			inline-end;">
+				<p align="center">
+					<iframe width="560"
+						height="315"
 						src="https://www.youtube.com/embed/uei7yrwQz4Y"
-						title="YouTube video player" 
-						frameborder="0" 
-						allow="accelerometer; 
-						       autoplay; 
-						       clipboard-write; 
-						       encrypted-media; 
-						       gyroscope; 
-						       picture-in-picture; 
-						       web-share"  
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer;
+						       autoplay;
+						       clipboard-write;
+						       encrypted-media;
+						       gyroscope;
+						       picture-in-picture;
+						       web-share"
 						allowfullscreen>
-</iframe>
-
+					</iframe>
+				</p>
+				/div>
 </div>
-			</div>
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-	</body>
+<script src="assets/js/jquery.min.js">
+</script>
+<script src="assets/js/browser.min.js">
+</script>
+<script src="assets/js/breakpoints.min.js">
+</script>
+<script src="assets/js/util.js">
+</script>
+<script src="assets/js/main.js">
+</script>
+</body>
 </html>
 
 <?php
